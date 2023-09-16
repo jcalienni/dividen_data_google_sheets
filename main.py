d457cb53cda48b42072a662f683cfc6a62cfcb42
jcalienni <juliancalienni@gmail.com>
2023-09-16 20:36:40 -0300

    added loggin, add dividend yield type calculation TTM FWD

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from dotenv import load_dotenv
+import logging
 
 
 def main():
@@ -49,15 +50,31 @@ def main():
                 d_pay_date = ticker["results"][-1]["pay_date"]
                 d_cash_amount = ticker["results"][-1]["cash_amount"]
                 d_div_frequency = ticker["results"][-1]["frequency"]
-                div_yield = round(d_cash_amount*d_div_frequency/d_current_price, 4)
+                div_yield = calculate_div_yield(ticker, d_div_frequency, d_current_price)
                 update_time = str(datetime.now())
                 sheet.values().update(spreadsheetId=SPREADSHEET_ID,
                                     range=f'{SHEET_NAME}!A{count+2}', valueInputOption = "USER_ENTERED", 
                                     body={"values": [[d_ticker, d_current_price, d_ex_dividend_date, d_pay_date, d_cash_amount, div_yield, update_time]]}).execute()
             if ((count+1)%5 == 0):
+                logging.info("polygon.io 5 request per minute exceeded. Sleep for 60 seconds.")
                 sleep(60)
     except HttpError as err:
         print(err)
+
+def calculate_div_yield(div_data, d_div_frequency, d_current_price, dividend_type="FWD"):
+    """
+    TTM: The sum of actual ordinary cash dividend payments in the last 12 months divided by the last close price
+    FWD: Last paid dividend multiplid by frequency divided current price
+    """
+    if dividend_type == "TTM":
+        sum_dividends = 0
+        for data in div_data["results"][-d_div_frequency:]:
+            sum_dividends += data["cash_amount"]
+        return round(sum_dividends/d_current_price, 4)
+    elif dividend_type == "FWD":
+        return round(div_data["results"][-1]["cash_amount"]*d_div_frequency/d_current_price, 4)
+    else:
+        raise("Unknown dividend type, select 'TTM' or 'FWD'")
 
 if __name__ == '__main__':
     main()
